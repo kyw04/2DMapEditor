ea282bd926653c6742af6dc5cdb835428cf3a8de
kyw04 <85551017+kyw04@users.noreply.github.com>
2025-12-18 20:51:27 +0900

    Update Drawable.cs

--- Assets/Scripts/MapEditor/Scriptable/Pencil/Drawable.cs
+++ Assets/Scripts/MapEditor/Scriptable/Pencil/Drawable.cs
@@ -18,18 +18,20 @@ namespace MapEditor.Pencil
         
         public GameObject Draw(Vector3 pos)
         {
+            if (EventSystem.current.IsPointerOverGameObject())
+                return null;
+         
             float x = Mathf.Round(pos.x);
             float y = Mathf.Round(pos.y);
-            pos = new Vector3(x, y);
-                
-            if (Physics2D.Raycast(pos, Vector3.forward) ||
-                EventSystem.current.IsPointerOverGameObject())
-                return null;
-
-            pos.z = 0;
-            return Render(pos);
+            pos = new Vector3(x, y, 0);
+            
+            if (Physics2D.Raycast(pos, Vector3.forward))
+                return ChangeRender(pos);
+            
+            return CreateRender(pos);
         }
 
-        protected abstract GameObject Render(Vector3 pos);
+        protected abstract GameObject CreateRender(Vector3 pos);
+        protected abstract GameObject ChangeRender(Vector3 pos);
     }
 }
--- Assets/Scripts/MapEditor/Scriptable/Pencil/SimplePencil.cs
+++ Assets/Scripts/MapEditor/Scriptable/Pencil/SimplePencil.cs
@@ -6,13 +6,18 @@ namespace MapEditor.Pencil
     [CreateAssetMenu(menuName = "ScriptableObjects/Pencil", fileName = "Pencil", order = 1)]
     public class SimplePencil : Drawable
     {
-        protected override GameObject Render(Vector3 pos)
+        protected override GameObject CreateRender(Vector3 pos)
         {
             var render = Instantiate(defaultObj, pos, Quaternion.identity).GetComponent<SpriteRenderer>();
             render.sprite = sprite;
             render.color = color;
 
             return render.gameObject;
+        }
+
+        protected override GameObject ChangeRender(Vector3 pos)
+        {
+            return null;
         }
     }
 }
